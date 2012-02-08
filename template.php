ded346fdc7d8246547791325c8cf11b43dfaf971
Guillaume Olivetti <contact@devduweb.com>
2012-02-08 07:48:11 +0100

    Marge template.php, with the header and footer

--- template.php
+++ template.php
@@ -1,26 +1,132 @@
-<?php if (!defined('IN_GS')) {die('you cannot load this page directly.');}
+<?php
 /**
- *
- * @package		General Green
- * @version		0.1
- * @desc		A theme for the GetSimple 3.0 CMS, from http://www.templatemo.com
- * @author		Guillaume Olivetti
- *
+ * header.inc.php The main theme file
+ * 
+ * @desc A theme for the GetSimple 3.0 CMS based on the theme General from http://www.templatemo.com
+ * @package General Green
+ * @author Guillaume Olivetti
+ * @version 0.1 
  */
-include('header.inc.php');
-?>
-<div class="main_content">    
-	<div id="main_column">
-		<article>
-			<heading>
-				<h2><?php get_page_title(); ?></h2>
-			</heading>
-			<?php get_page_content(); ?>
-		</article>
-	</div>
-	
-	<?php include('sidebar.inc.php'); ?>
+if (!defined('IN_GS')) { die('you cannot load this page directly.'); }
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title><?php get_page_clean_title(); ?>-<?php get_site_name(); ?></title>
+		<?php get_header(); ?>
+		<meta name="robots" content="index, follow">
+		<link href="<?php get_theme_url(); ?>/style.css" rel="stylesheet" type="text/css">
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
 
-</div>
+	<body id="<?php get_page_slug(); ?>">
+		<div class="container">
+			<nav>
+				<ul>
+					<?php get_navigation(return_page_slug()); ?>
+				</ul>
+			</nav>
 
-<?php include('footer.inc.php'); ?>
+			<header>
+				<heading>
+					<h1>
+						<a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a>
+					</h1>
+					<h2><?php echo BOLTGUN_HEADER_SUBTITLE; ?></h2>
+				</heading>
+
+				<div id="search_box">
+				</div>
+			</header>
+
+			<div class="main_content">    
+				<div id="main_column">
+					<article>
+						<heading>
+							<h2><?php get_page_title(); ?></h2>
+						</heading>
+				<?php get_page_content(); ?>
+					</article>
+				</div>
+
+			<?php include('sidebar.inc.php'); ?>
+
+			</div>
+
+			<?php
+			// Hidden till more content is made
+			if (FALSE) : ?>
+			<footer>
+
+				<div class="footer_box m_right">
+					<div class="footer_bottom"></div>
+
+					<h4>Lorem ipsum dolor</h4>
+
+					<div class="footer_box_content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam vitae ipsum vulputate varius vitae semper nunc.</p>
+						<div class="cleaner_h10"></div>
+						<ul class="list_01">
+							<li><a href="#">Proin nec molestie ipsum</a></li>
+							<li><a href="#">Curabitur ut mattis urna</a></li>
+							<li><a href="#">Etiam luctus arcu at leo</a></li>
+							<li><a href="#">Integer sit amet tortor vel diam </a></li>
+						</ul>
+
+						<div class="button_01"><a href="#">View All</a></div>
+					</div>            
+
+				</div>
+
+				<div class="footer_box m_right">
+					<div class="footer_bottom"></div>
+
+					<h4>Maecenas urna lorem</h4>
+
+					<div class="footer_box_content">
+						<p>Morbi dictum semper varius. Quisque nec purus erat, vitae sodales urna. Integer aliquam sapien vitae turpis .</p>
+						<div class="cleaner_h10"></div>
+						<ul class="list_01">
+							<li><a href="#">Nullam porttitor tellus ut turpis</a></li>
+							<li><a href="#">Mauris lobortis nisl id lorem</a></li>
+							<li><a href="#">Vivamus at mi a sem aliquet</a></li>
+							<li><a href="#">Etiam tempus tellus eget est</a></li>
+						</ul>
+						<div class="button_01"><a href="#">View All</a></div>                
+					</div>            
+
+				</div>
+
+				<div class="footer_box">
+					<div class="footer_bottom"></div>
+
+					<h4>Donec eleifend</h4>
+
+					<div class="footer_box_content">
+						<p>Integer bibendum quam sagittis neque consectetur eleifend. Integer feugiat magna sit amet leo rhoncus elementum.</p>
+						<div class="cleaner_h10"></div>
+						<ul class="list_01">
+							<li><a href="#">Proin nec molestie ipsum</a></li>
+							<li><a href="#">Vivamus ornare ornare leo</a></li>
+							<li><a href="#">Nulla porta vehicula pulvinar</a></li>
+							<li><a href="#">Duis ac eros quam</a></li>
+						</ul>
+						<div class="button_01"><a href="#">View All</a></div>                
+					</div>            
+
+				</div>
+
+			</footer>
+			<?php endif; ?>
+
+			<footer class="footer_bar">
+				Copyright © 2012 <a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a>
+			</footer>
+
+<?php get_footer(); ?>
+
+		</div>
+	</body>
+</html>
